9562bc67be774d2ea18c3b1173358dec86519a61
da.moura <da.moura@campus.fct.unl.pt>
2020-03-22 16:09:53 +0000

    BotWorking

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Twit = require('twit');
+const config = require('./config.js');
+const tweetBot = new Twit(config);
+
+
+var CronJob = require('cron').CronJob;
+var job = new CronJob('0 0 16 * * *', function () {
+    let status = '';
+    if (config.language === 'PT') {
+        status = config.can_i_leave === 'FALSE' ? 'Não.' : 'Sim.';
+    }
+    else if (config.language === 'EN') {
+        status = config.can_i_leave === 'FALSE' ? 'No.' : 'Yes.';
+    }
+    if (status !== '') {
+        tweetBot.post('statuses/update', { status }, function (err, data, response) {
+            console.log(data)
+        })
+    }
+}, null, true, 'Europe/Lisbon');
+job.start();
